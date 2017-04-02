ab021de77093d0c8c444141554d01fbcdc08db10
Jane <janewyx11@gmail.com>
2017-04-02 16:38:43 -0700

    cleaning codes

--- gaussian.R
+++ gaussian.R
@@ -3,11 +3,15 @@ library(ggplot2)
 
 setwd("H:/geob/402_Air_Pollution_Meteorology/Assg_3/")
 
+## reading in data with columns of z, effective stack height, and stability class.
 gau_df <- read.csv("gaussian.csv")
 
 ## inserting new columns for sigma values
 gau_df$sigmay <- NA
 gau_df$sigmaz <- NA
+
+## for this study, crosswind y component is 0
+y <- 0
 
 ## define sigma value based on Gifford (12) for z between 100 and 10000m
 ## class A:
@@ -52,12 +56,12 @@ sigmaz_F <- 0.016*10000*(1+0.0003*10000)^(-1)
 gau_df$sigmay[gau_df$class == "F"] <- sigmay_F
 gau_df$sigmaz[gau_df$class == "F"] <- sigmaz_F
 
-## computing Gaussian concentration for each class
+## computing Gaussian concentration for each class and inputting results in dataframe
 gau_df <- gau_df %>% 
   group_by(class) %>% 
-  mutate(conc = 1000000*5/(2*pi*1*sigmay*sigmaz)*(exp((-(z-Effective.Stack.Height)^2)/(2*sigmaz^2)) + exp(-(z+Effective.Stack.Height)^2/(2*sigmaz^2)))*exp((0)))
+  mutate(conc = 1000000*5/(2*pi*1*sigmay*sigmaz)*(exp((-(z-Effective.Stack.Height)^2)/(2*sigmaz^2)) + exp(-(z+Effective.Stack.Height)^2/(2*sigmaz^2)))*exp(-(y^2)/(2*sigmay)))
                                      
-## plotting
+## plotting modelled concentrations for different classes at different heights
 ggplot(gau_df, aes(conc, z, group = class, colour = class)) +
   xlab(expression(Concentration ~(mu~g/m^{3}))) +
   ylab("Height (m)") +
